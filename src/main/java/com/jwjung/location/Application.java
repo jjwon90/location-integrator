1d12c7d28e5620fef2c1f5e8599382f1ffd58a78
juwon.jung <jjwonypo@gmail.com>
2023-02-09 17:03:58 +0900

    initial commit

--- src/main/java/com/jwjung/location/Application.java
+++ src/main/java/com/jwjung/location/Application.java
@@ -1,9 +1,15 @@
 package com.jwjung.location;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,4 +20,12 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper()
+                .configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.USE_LONG_FOR_INTS, true);
+        objectMapper.setSerializationInclusion(NON_NULL);
+        return objectMapper;
+    }
 }
